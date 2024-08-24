b374bb98457f24fe9974ad9d4c25a0ef6ab89c28
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-24 13:36:10 +0530

    Update TestCustomLoginException.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exception/TestCustomLoginException.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exception/TestCustomLoginException.java
@@ -1,7 +1,9 @@
 package com.sunilbooks.selflearnjava.exception;
 
 /**
- * Tests custom LoginException class.
+ * The {@code TestCustomLoginException} class demonstrates the usage of a custom
+ * {@code LoginException}. It attempts to authenticate a user and throws a 
+ * {@code LoginException} if the authentication fails.
  * 
  * @version 1.0
  * @since 16 Nov 2014
@@ -9,24 +11,30 @@ package com.sunilbooks.selflearnjava.exception;
  * @Copyright (c) Sunil Sahu
  * @url www.sunilbooks.com
  */
-
 public class TestCustomLoginException {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		try {
-			authenticate("SUNRAYS", "SUNRAYS");
-		} catch (LoginException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+        try {
+            authenticate("SUNRAYS", "SUNRAYS");
+        } catch (LoginException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-	public static void authenticate(String login, String pwd)
-			throws LoginException {
-		boolean flag = true;
-		if (flag) {
-			throw new LoginException();
-		}
-	}
+    /**
+     * Authenticates the user based on the provided login and password.
+     * 
+     * @param login The login ID of the user.
+     * @param pwd   The password of the user.
+     * @throws LoginException if the authentication fails.
+     */
+    public static void authenticate(String login, String pwd)
+            throws LoginException {
+        boolean flag = true;
+        if (flag) {
+            throw new LoginException();
+        }
+    }
 
 }
